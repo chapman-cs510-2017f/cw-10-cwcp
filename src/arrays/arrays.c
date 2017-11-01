b2fe709da6bdefd8d56428023f6503fa78afe58e
cwatkins1123 <watki115@mail.chapman.edu>
2017-11-01 04:55:50 +0000

    :)

--- src/arrays/arrays.c
+++ src/arrays/arrays.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include "arrays.h"
 
-void set_arrays(int array_size, int array[array_size], long double *memblock);
-void print_arrays(int array_size, int array[array_size], long double *memblock,const char *foo);
-
-
 void set_arrays(int array_size, int array[array_size], long double *memblock) 
 {
        /* Fill declared arrays with integers */

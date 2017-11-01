4a26c56c2d9d8a348d4ab5362d33c2c759c150d2
cmparlettpelleriti <parlett@chapman.edu>
2017-11-01 04:33:00 +0000

    arrays

--- src/arrays/arrays.c
+++ src/arrays/arrays.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "arrays.h"
+
+void set_arrays(int array_size, int array[array_size], long double &memblock, int i);
+void print_arrays(int array_size, int array[array_size], long double &memblock,const char *foo, int i);
+
+
+void set_arrays(int array_size, int array[array_size], long double &memblock, int i) 
+{
+       /* Fill declared arrays with integers */
+    for (i=0; i < array_size; i++)
+    {
+        // assign the value i to the ith element 
+        array[i] = i;
+        
+        // set the value inside the memory address at memblock + (i bytes) to i
+        *(memblock+i) = i;
+    }
+    /* End fill with integers */
+
+}
+
+void print_arrays(int array_size, int array[array_size], long double &memblock,const char *foo, int i)
+{
+        for (i=0; i < (array_size + 3); i++)
+    {
+        // print the array values using array syntax
+        printf("array[%d] : %d\t", i, array[i]);
+        
+        // print values contained in each memory address starting at memblock 
+        printf("*(memblock + %d) : %Lf\t", i, *(memblock+i));
+        
+        // print each character in the string using array syntax
+        printf("foo[%d] : %c\t", i, foo[i]);
+        
+        // do the same thing as above, but in memory pointer notation
+        printf("*(foo + %d) : %c\n", i, *(foo+i));
+    } 
+}
 
 int main(void)
 {
@@ -14,7 +52,7 @@ int main(void)
     
     // Allocate a block of array_size integers and assign the address
     // of the beginning of the memory block to the pointer memblock
-    int *memblock = malloc(array_size * sizeof(int));
+    long double *memblock = malloc(array_size * sizeof(int));
     /* WARNING: malloc may fail and return a NULL value for the pointer
                 Good programming practice mandates checking for such failures.
     */
@@ -30,37 +68,10 @@ int main(void)
     /* String example showing the two methods are equivalent */
     // constant character array (i.e., a string)
     const char *foo = "Foo bar .";
-    /* End string declaration as array of characters */
     
-    /* Fill declared arrays with integers */
-    for (i=0; i < array_size; i++)
-    {
-        // assign the value i to the ith element 
-        array[i] = i;
-        
-        // set the value inside the memory address at memblock + (i bytes) to i
-        *(memblock+i) = i;
-    }
-    /* End fill with integers */
+    set_arrays(array_size,array,&memblock, i);
+    print_arrays(array_size,array,&memblock,&foo, i);
 
-    /* Print out results to verify exactly what the above code did */
-
-    // Note: this increments three bytes BEYOND the allocated memory (buffer overrun)
-    for (i=0; i < (array_size + 3); i++)
-    {
-        // print the array values using array syntax
-        printf("array[%d] : %d\t", i, array[i]);
-        
-        // print values contained in each memory address starting at memblock 
-        printf("*(memblock + %d) : %d\t", i, *(memblock+i));
-        
-        // print each character in the string using array syntax
-        printf("foo[%d] : %c\t", i, foo[i]);
-        
-        // do the same thing as above, but in memory pointer notation
-        printf("*(foo + %d) : %c\n", i, *(foo+i));
-    }
-    
     // explicitly free the block of memory malloc-ed at memblock 
     free(memblock);
 
---
+++ src/arrays/arrays.h
@@ -0,0 +1,3 @@
+
+void set_arrays(int array_size, int array[array_size], long double &memblock, int i);
+void print_arrays(int array_size, int array[array_size], long double &memblock,const char *foo, int i);
